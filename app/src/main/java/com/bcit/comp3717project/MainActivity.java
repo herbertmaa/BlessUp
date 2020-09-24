9b019bca60e1ee2a9b909dab8e4d17f88a21ab2d
herbertma <hma49@sfu.ca>
2020-09-24 15:32:55 -0700

    added gradle configuration to include MongoDB Library

--- app/src/main/java/com/bcit/comp3717project/MainActivity.java
+++ app/src/main/java/com/bcit/comp3717project/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MongoClientURI uri = new MongoClientURI(
+                "mongodb+srv://admin:<password>@cluster1.8lepj.mongodb.net/<dbname>?retryWrites=true&w=majority");
+
+        MongoClient mongoClient = new MongoClient(uri);
+        MongoDatabase database = mongoClient.getDatabase("test");
+
     }
 }
