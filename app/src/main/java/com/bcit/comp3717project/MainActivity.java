385626fce3bb55aaa81b00a271141d66205b2241
herbertma <hma49@sfu.ca>
2020-09-25 16:52:48 -0700

    updated some small things, still tesitng
    Signed-off-by: herbertma <hma49@sfu.ca>

--- app/src/main/java/com/bcit/comp3717project/MainActivity.java
+++ app/src/main/java/com/bcit/comp3717project/MainActivity.java
@@ -3,18 +3,16 @@ package com.bcit.comp3717project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import io.realm.Realm;
-import io.realm.RealmConfiguration;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     String appID = BuildConfig.APPLICATION_ID;
     Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Realm.init(getApplicationContext());
         this.realm = Realm.getDefaultInstance();
 
