f19e829dde07836104d30bf6b0d9fab1a106473d
Micheal Ozdoba <mjozdoba@gmail.com>
2020-09-26 13:16:37 -0700

    Religion Selection Activity
    Added PNGs
    Added Buttons
    Constraint Layouts

---
+++ app/src/main/java/com/bcit/comp3717project/ReligionSelectionActivity.java
@@ -0,0 +1,15 @@
+package com.bcit.comp3717project;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ReligionSelectionActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_religion_selection);
+    }
+}
