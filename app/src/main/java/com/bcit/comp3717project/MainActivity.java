048397f481431ca144d83325061a4f53a5047e90
Mozdoba <mjozdoba@gmail.com>
2020-10-08 16:54:03 -0700

    Register Layout and Activity (#11)
    * Added misc.xml to git ignore and fixed Religion Selection Intent
    * Register Layout & Activity

--- app/src/main/java/com/bcit/comp3717project/MainActivity.java
+++ app/src/main/java/com/bcit/comp3717project/MainActivity.java
@@ -20,17 +20,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSelectReligionClick(View view) {
-        Intent i = new Intent(this, ReligionSelectionActivity.class); //TODO
+        Intent i = new Intent(this, ReligionSelectionActivity.class);
         startActivity(i);
     }
 
     public void onSelectChatClick(View view) {
         Intent i = new Intent(this, ChatActivity.class);
         startActivity(i);
-
     }
+
     public void onSelectChurchClick(View view) {
         Intent i = new Intent(this, ChurchListingsActivity.class);
+        startActivity(i);
+    }
+
+    public void onSelectRegisterClick(View view) {
+        Intent i = new Intent(this, RegisterActivity.class);
         startActivity(i);
     }
 
