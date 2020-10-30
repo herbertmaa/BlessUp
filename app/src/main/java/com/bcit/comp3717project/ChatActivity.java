338100d725216def8409fc906b3e292875008b85
Mozdoba <mjozdoba@gmail.com>
2020-10-29 21:12:45 -0700

    Firebase Chat (#19)
    * Added misc.xml to git ignore and fixed Religion Selection Intent
    
    * Register Layout & Activity
    
    * Added Class Skeletons
    
    * UserType
    
    * Chat Message Firebase Works
    
    * Firebase Chat

--- app/src/main/java/com/bcit/comp3717project/ChatActivity.java
+++ app/src/main/java/com/bcit/comp3717project/ChatActivity.java
@@ -1,16 +1,120 @@
 package com.bcit.comp3717project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import model.ChatListAdapter;
+import model.ChatMessage;
+import model.User;
 
 public class ChatActivity extends AppCompatActivity {
 
     private static final String TAG = "ChatActivity";
+    private EditText editTextMessage;
+    private Button buttonSendMessage;
+
+    ListView lvChat;
+    List<ChatMessage> messagesList;
+
+    DatabaseReference databaseChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        editTextMessage = findViewById(R.id.editTextMessage);
+        buttonSendMessage = findViewById(R.id.btnSendMessage);
+
+        databaseChat = FirebaseDatabase.getInstance().getReference("chat");
+
+        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+
+        lvChat = (ListView) findViewById(R.id.lvChat);
+        messagesList = new ArrayList<ChatMessage>();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        databaseChat.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                messagesList.clear();
+                for (DataSnapshot toDoItemSnapshot : dataSnapshot.getChildren()) {
+                    ChatMessage chatMessage = toDoItemSnapshot.getValue(ChatMessage.class);
+                    messagesList.add(chatMessage);
+                }
+
+                ChatListAdapter adapter = new ChatListAdapter(ChatActivity.this, messagesList);
+                lvChat.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
+    }
+
+    private void sendMessage() {
+        String message = editTextMessage.getText().toString();
+
+        if (TextUtils.isEmpty(message)) {
+            Toast.makeText(this, "You must enter a message.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String messageFireBaseID = databaseChat.push().getKey();
+
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        String createdAt = "" + day + "-" + month + "-" + year;
+        ChatMessage chatMessage = new ChatMessage(messageFireBaseID, message, createdAt);
+
+        Task setValueTask = databaseChat.child(messageFireBaseID).setValue(chatMessage);
+
+        setValueTask.addOnSuccessListener(new OnSuccessListener() {
+            @Override
+            public void onSuccess(Object o) {
+                Toast.makeText(ChatActivity.this,"Message Sent.",Toast.LENGTH_LONG).show();
+                editTextMessage.setText("");
+            }
+        });
+
+        setValueTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ChatActivity.this,
+                        "Something went wrong.\n" + e.toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/model/ChatListAdapter.java
@@ -0,0 +1,52 @@
+package model;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.bcit.comp3717project.R;
+
+import java.util.List;
+
+public class ChatListAdapter extends ArrayAdapter<ChatMessage> {
+    private Activity context;
+    private List<ChatMessage> messageList;
+
+    public ChatListAdapter(Activity context, List<ChatMessage> messageList) {
+        super(context, R.layout.activity_chat, messageList);
+        this.context = context;
+        this.messageList = messageList;
+    }
+
+    public ChatListAdapter(Context context, int resource, List<ChatMessage> objects, Activity context1, List<ChatMessage> messageList) {
+        super(context, resource, objects);
+        this.context = context1;
+        this.messageList = messageList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.chatlist_layout, null, true);
+
+        TextView tvName = listViewItem.findViewById(R.id.textViewName);
+        TextView tvMessage = listViewItem.findViewById(R.id.textViewMessage);
+        TextView tvCreatedOn = listViewItem.findViewById(R.id.textViewCreatedOn);
+
+        ChatMessage chatMessage = messageList.get(position);
+        String firstName = "HAM DEMO";
+        tvName.setText(firstName);
+        tvMessage.setText(chatMessage.getMessage());
+        tvCreatedOn.setText(chatMessage.getCreatedAt());
+
+        return listViewItem;
+    }
+}
