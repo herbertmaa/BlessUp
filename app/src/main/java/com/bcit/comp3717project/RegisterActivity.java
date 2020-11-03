a08004d7987832c13c6585af6f0e472c3fe515b2
RoyArka <arkaroy0999@gmail.com>
2020-11-02 19:14:10 -0800

    Redid Register layout and commented out codeblock that checks if user is loggedin/registered for testing. Commented out startactivity for testing.

--- app/src/main/java/com/bcit/comp3717project/RegisterActivity.java
+++ app/src/main/java/com/bcit/comp3717project/RegisterActivity.java
@@ -56,7 +56,7 @@ public class RegisterActivity extends AppCompatActivity {
         fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
             if(task.isSuccessful()){
                 Toast.makeText(RegisterActivity.this, "User Created", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//                startActivity(new Intent(getApplicationContext(), MainActivity.class));
             } else {
                 Toast.makeText(RegisterActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
             }
