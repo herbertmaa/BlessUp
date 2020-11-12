b0504e504bf0382efa2432b473713054167e8773
Micheal Ozdoba <mjozdoba@gmail.com>
2020-11-12 14:38:55 -0800

    Chat Messages are associated with Users. Shared Preferences is able to handle some aspects of Session Handling.

--- app/src/main/java/com/bcit/comp3717project/RegisterActivity.java
+++ app/src/main/java/com/bcit/comp3717project/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.bcit.comp3717project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,14 +9,20 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
 
 import model.User;
 
 
 public class RegisterActivity extends FireBaseActivity {
+
+    SharedPreferences pref;
 
     //widgets and firebaseauth
     EditText FullName, UserEmail, Password, PasswordConfirm;
@@ -28,6 +35,9 @@ public class RegisterActivity extends FireBaseActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        pref = getSharedPreferences("user_details", MODE_PRIVATE);
+
         FullName = findViewById(R.id.FullName);
         UserEmail = findViewById(R.id.UserEmail);
         Password = findViewById(R.id.emailPasswordTextField);
@@ -44,22 +54,35 @@ public class RegisterActivity extends FireBaseActivity {
 
     public void register(View view) {
 
-        String fullName = findViewById(R.id.FullName).toString().trim();
+        String fullName = FullName.getText().toString().trim();
         String email = UserEmail.getText().toString().trim();
         String password = Password.getText().toString().trim();
 
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
             if(task.isSuccessful()){
-                FirebaseUser user = auth.getCurrentUser();
-                String userID = user.getUid();
+                FirebaseUser firebaseUser = auth.getCurrentUser();
+                updateUserDisplayName(fullName, firebaseUser);
+
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("email", email);
+                editor.putString("displayName", fullName);
+                editor.commit();
+
+                String userID = firebaseUser.getUid();
                 User newUser = new User(userID, fullName, fullName, email);
                 DatabaseReference usersReference = FirebaseDatabase.getInstance().getReference("users");
                 usersReference.child(userID).setValue(newUser);
                 Toast.makeText(RegisterActivity.this, "User Created", Toast.LENGTH_SHORT).show();
-
             } else {
                 Toast.makeText(RegisterActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void updateUserDisplayName(String displayName, FirebaseUser user) {
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(displayName)
+                .build();
+        user.updateProfile(profileUpdates);
     }
 }
