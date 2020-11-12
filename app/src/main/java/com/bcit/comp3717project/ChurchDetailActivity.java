72b4dc5bb0746442b1c6162df07b604d4ebd91ce
Herbert Ma <38601017+herbertmaa@users.noreply.github.com>
2020-11-12 15:49:48 -0800

    added pageview, please update your workspace for required API keys

---
+++ app/src/main/java/com/bcit/comp3717project/ChurchDetailActivity.java
@@ -0,0 +1,47 @@
+package com.bcit.comp3717project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import model.Church;
+
+public class ChurchDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_church_detail);
+        displayChurchDetails();
+    }
+
+
+    private void displayChurchDetails(){
+        Church church = (Church) getIntent().getExtras().get("church");
+
+        if(church != null){
+
+            Log.e("TEST","IN IF STATEMENT");
+            TextView church_name = findViewById(R.id.church_name);
+            church_name.setText(church.getName());
+
+            TextView church_description = findViewById(R.id.church_description);
+            church_description.setText(church.getDescription());
+
+            TextView church_address = findViewById(R.id.church_address);
+            church_address.setText(church.getAddress());
+
+            TextView church_number = findViewById(R.id.church_number);
+            church_address.setText(church.getPhoneNumber());
+//            ImageView church_image = findViewById(R.id.church_image);
+//            church_image.setImageDrawable(ContextCompat.getDrawable(this,);
+//            church_image.setContentDescription(church.getName());
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/bcit/comp3717project/ViewChurchesActivity.java
@@ -0,0 +1,66 @@
+package com.bcit.comp3717project;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewChurchesActivity extends AppCompatActivity {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+
+    public class SectionsPageAdapter extends FragmentPagerAdapter {
+
+        private int count = 0;
+        public SectionsPageAdapter(FragmentManager fm) { super(fm); }
+
+        @Override
+        public int getCount() { return count; }
+
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+            count++;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_churches);
+
+        TabLayout tabLayout = findViewById(R.id.tableLayout);
+        ViewPager pager = findViewById(R.id.pager);
+        tabLayout.setupWithViewPager(pager);
+
+        SectionsPageAdapter pagerAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new ListChurches(), "List");
+        pagerAdapter.addFragment(new MapChurches(), "Map");
+        Log.e("VIEWCHURCHES", "SHOULD OF ADDED MAPCHURCHES?");
+        pager.setAdapter(pagerAdapter);
+    }
+}
