c06b2eba618b4c1a699a8a902954714a64f95ae8
herbertma <hma49@sfu.ca>
2020-11-25 15:24:22 -0800

    tried updating a bit
    Signed-off-by: herbertma <hma49@sfu.ca>

--- app/src/main/java/fragment/MapChurches.java
+++ app/src/main/java/fragment/MapChurches.java
@@ -2,6 +2,7 @@ package fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -20,6 +21,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -87,32 +93,31 @@ public class MapChurches extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.googleMap);
         mapFragment.getMapAsync(this);
 
-        //TODO retrieve from firebase and then create these churches
         ArrayList<Church> churches = new ArrayList<>();
+        DatabaseReference churchesReference = FirebaseDatabase.getInstance().getReference("churches");
+        churchesReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot churchSnapshot : snapshot.getChildren()) {
+                    Church church = churchSnapshot.getValue(Church.class);
+                    churches.add(church);
+                }
+                Church[] church_array = churches.toArray(new Church[churches.size()]);
+                ChurchAdapter adapter = new ChurchAdapter(church_array, false);
+                mapRecycler.setAdapter(adapter);
+                GridLayoutManager lm = new GridLayoutManager(getView().getContext(), 1);
+                mapRecycler.setLayoutManager(lm);
 
-        Church c1 = new Church("123", "Willingdon Church", "4812 Willingdon Ave, Burnaby, BC V5G 3H6", "604-435-5544", "Christianity", "Willingdon Church is a community church; international in attendance, biblical in its message, uplifting in its worship and committed in its service to all ages ");
-        Church c2 = new Church("234", "Renfrew Baptist Church", "1899 Renfrew St, Vancouver, BC V5M 3J1", "604-253-2089", "Christianity", "The church is located in a cosmopolitan area of Vancouver close to a variety of ethnic groups. To this end we seek to reach out to our local neighborhood to share the Gospel of Jesus Christ .. a message of central importance to every soul. We desire to share that Gospel message with you. Come visit us! ");
-        Church c3 = new Church("345", "Pacific Grace MB Church Vancouver", "2855 E 1st Ave, Vancouver, BC V5M 1A9", "604-255-6199", "Christianity", "Pacific Grace MB Church （基督教頌恩堂） began as Pacific Grace Mission Chapel at 1587 Frances Street in Vancouver, with its first fellowship on 11 December 1963. By 1964, a congregation was formed, worshiping in English and German, with a few ethnic Chinese participating. It was determined to reach all nearby residents, including the Chinese in Chinatown.");
-        Church c4 = new Church("456", "West Coast Christian Fellowship", "3198 Georgia Street East, Vancouver, BC V5K 2L1", "604-255-7301", "Christianity", "We're a Christian church in the Hastings-Sunrise neighbourhood of East Vancouver, just two blocks away from PNE Playland. We'd love you to come visit.");
-        Church c5 = new Church("567", "Broadway Church", "2700 E Broadway, Vancouver, BC V5M 1Y8", "604-253-2700", "Christianity", "We exist to produce fully devoted followers of Jesus Christ. We desire to be a non-threatening community where one can both seek and grow in Christ.");
-        Church c6 = new Church("678", "City Baptist Church", "2775 E Hastings St, Vancouver, BC V5K 1Z8", "604-562-0887", "Christianity", "We exist to share the life changing love of Jesus with Vancouver and beyond. We invite to worship with City Baptist Church in Vancouver. We are passionate about teaching the life changing truth of the Bible and sharing that truth in our community.");
 
-        churches.add(c1);
-        churches.add(c2);
-        churches.add(c3);
-        churches.add(c4);
-        churches.add(c5);
-        churches.add(c6);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
-        Church[] church_array = churches.toArray(new Church[churches.size()]);
-        ChurchAdapter adapter = new ChurchAdapter(church_array, false);
-
-        mapRecycler.setAdapter(adapter);
-
-        GridLayoutManager lm = new GridLayoutManager(getView().getContext(), 1);
-
-        mapRecycler.setLayoutManager(lm);
     }
 
     @Override
