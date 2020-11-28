beae8689e61c44f6c4cc22ed2ff3ac53fafde2e2
Micheal Ozdoba <mjozdoba@gmail.com>
2020-11-28 15:54:12 -0800

    More Comments

--- app/src/main/java/model/Church.java
+++ app/src/main/java/model/Church.java
@@ -7,91 +7,116 @@ import java.util.HashMap;
 
 public class Church implements Serializable {
 
+    /**
+     * The ID used to Query this Church object from Firebase
+     */
     private String churchID;
+
+    /**
+     * The name of this church
+     */
     private String name;
+
+    /**
+     * This church's address
+     */
     private String address;
+
+    /**
+     * This church's phone number
+     */
     private String phoneNumber;
-    private String religion;
+
+    /**
+     * This church's description
+     */
     private String description;
+
+    /**
+     * The image url that contains a photo of this church
+     */
     private String imageURL;
     public HashMap<String, User> members;
 
+    /**
+     * Gets the image's URL in FirebaseStore
+     * @return
+     */
     public String getImageURL() {
         return imageURL;
     }
 
-    public void setImageURL(String imageURL) {
-        this.imageURL = imageURL;
-    }
-
+    /**
+     * Default Constructor
+     */
     private Church() {}
+
     public Church(String churchID, String name, String address, String phoneNumber, String religion, String description) {
         this.churchID = churchID;
         this.name = name;
         this.address = address;
         this.phoneNumber = phoneNumber;
-        this.religion = religion;
         this.description = description;
         this.members = new HashMap<>();
         this.imageURL = null;
     }
 
+    /**
+     *
+     * @return - The ID used to Query this Church object from Firebase
+     */
     public String getChurchID() {
         return churchID;
     }
 
-    public void setChurchID(String churchID) {
-        this.churchID = churchID;
-    }
-
+    /**
+     * @return - The Name of this Church
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name  - The Name of this Church
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return - This church's address
+     */
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
+    /**
+     * @return - this church's phone number
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getReligion() {
-        return religion;
-    }
-
-    public void setReligion(String religion) {
-        this.religion = religion;
-    }
-
+    /**
+     * @return - This church's description
+     */
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
+    /**
+     * @return - a list of members belonging to this church
+     */
     public HashMap<String,User> getMembers() {
         return members;
     }
 
-    public void setMembers(HashMap<String, User> members) {
-        this.members = members;
-    }
-
+    /**
+     * If there are no members to this church, creates a new Members array, else
+     * adds a member to the existing array.
+     * @param user
+     */
     @Exclude
     public void addUser(User user) {
         if(members == null) {
