9d56cf3552893c3761e73b9ec70e03dc41959d4b
Mozdoba <mjozdoba@gmail.com>
2020-11-28 19:24:12 -0800

    Micheal comments (#82)
    * Organized and commented Chat Activity and Chat Channel Activity
    
    * Added Comments for Adapters
    
    * More Comments
    
    * Chat Object Comments
    
    * Chat Message Comments
    
    * More Comments
    
    Co-authored-by: Herbert Ma <38601017+herbertmaa@users.noreply.github.com>

--- app/src/main/java/model/ChatMessage.java
+++ app/src/main/java/model/ChatMessage.java
@@ -3,17 +3,41 @@ package model;
 import java.util.Calendar;
 import java.util.Date;
 
-
 /**
- * This class represents the information pertaining to a chat message
+ * Chat Message that stores details about the user who owns the message,
+ *  what string was sent, the time the message was created, and if the message
+ *  was an image, or text file.
  */
 public class ChatMessage {
-    private String messageID; // Unique message id generated from database
-    private String message; // The message
-    private User createdBy; // The user ID that created this message
-    private Date createdAt; // The date this message was created
-    private String messageType; // The type of message
+    /**
+     * The ID used to query for this message object in Firebase
+     */
+    private String messageID;
 
+    /**
+     * The string that was sent to the Chat Channel
+     */
+    private String message;
+
+    /**
+     * The user who sent the message
+     */
+    private User createdBy;
+
+    /**
+     * The time the message was sent
+     */
+    private Date createdAt;
+
+    /**
+     * The format of the message
+     */
+    private String messageType;
+
+
+    /**
+     * No Param Constructor
+     */
     public ChatMessage() {};
 
     public ChatMessage(String messageID, String message, User user) {
@@ -23,42 +47,77 @@ public class ChatMessage {
         this.createdAt = Calendar.getInstance().getTime();
     }
 
+    /**
+     * @return The ID used to query for this message object in Firebase
+     */
     public String getMessageID() {
         return messageID;
     }
 
+    /**
+     * @param messageID - The ID used to query for this message object in Firebase
+     */
     public void setMessageID(String messageID) {
         this.messageID = messageID;
     }
 
+    /**
+     *
+     * @return - The string that was sent to the Chat Channel
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     *
+     * @param message - The string that was sent to the Chat Channel
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     *
+     * @return - The user who sent the message
+     */
     public User getCreatedBy() {
         return createdBy;
     }
 
+    /**
+     *
+     * @param createdBy - The user who sent the message
+     */
     public void setCreatedBy(User createdBy) {
         this.createdBy = createdBy;
     }
 
+    /**
+     * @return - The time the message was sent
+     */
     public Date getCreatedAt() {
         return createdAt;
     }
 
+    /**
+     *
+     * @param createdAt - The time the message was sent
+     */
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
     }
 
+    /**
+     * @return - The format of the message (Image or String)
+     */
     public String getMessageType() {
         return messageType;
     }
 
+    /**
+     * @param messageType - The format of the message (Image or String)
+     */
     public void setMessageType(String messageType) {
         this.messageType = messageType;
     }
--- app/src/main/java/model/User.java
+++ app/src/main/java/model/User.java
@@ -23,124 +23,37 @@ public class User implements Serializable {
     private String userType; // The user type
     private int age = 0; // The user age
 
-    public User() {
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
+    /**
+     * Default Constructor
+     */
+    public User() {}
 
     public User(String userID, String firstName, String lastName, String emailAddress) {
         this.userID = userID;
         this.firstName = firstName;
-        this.lastName = lastName;
-        this.emailAddress = emailAddress;
         this.joinDate = Calendar.getInstance().getTime().toString();
-        this.userType = "MEMBER";
-        this.lastActive = joinDate;
-        this.religion = "NONE";
     }
 
+    /**
+     * @return - The ID used to query for this User object from Firebase
+     */
     public String getId() {
         return userID;
     }
 
+    /**
+     * @param userID - The ID used to query for this User object from Firebase
+     */
     public void setId(String userID) {
         this.userID = userID;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    /**
+     * @return -  The user's first name
+     */
     public String getFirstName() {
         return firstName;
     }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getJoinDate() {
-        return joinDate;
-    }
-
-    public void setJoinDate(String joinDate) {
-        this.joinDate = joinDate;
-    }
-
-    public String getLastActive() {
-        return lastActive;
-    }
-
-    public void setLastActive(String lastActive) {
-        this.lastActive = lastActive;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getReligion() {
-        return religion;
-    }
-
-    public void setReligion(String religion) {
-        this.religion = religion;
-    }
-
-    public String getUserType() {
-        return userType;
-    }
-
-    public void setUserType(String userType) {
-        this.userType = userType;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public String getUserID() {
-        return userID;
-    }
-
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-
 
 }
 
